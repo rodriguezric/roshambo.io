10bbfa76d50dd7bd02bce64b6a7e66d080deccb8
Ric <rohins@ubuntu.members.linode.com>
2016-07-10 00:02:13 +0000

    Room::listRooms lists all rooms in redis

--- app/Room.php
+++ app/Room.php
@@ -5,14 +5,19 @@ namespace App;
 use Illuminate\Support\Facades\Redis;
 use Illuminate\Database\Eloquent\Model;
 
-/**
- * Create a room in Redis with name $name.
- * @param string $name Name of the room.
- **/
 class Room extends Model
 {
+    /**
+     * Create a room in Redis with name $name.
+     * @param string $name Name of the room.
+     **/
     public function createRoom($name)
     {
         Redis::sadd('rooms', $name);
+    }
+    
+    public function listRooms()
+    {
+        return Redis::smembers('rooms');
     }
 }
