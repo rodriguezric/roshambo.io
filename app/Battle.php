b5ba156cbcf9bda10d357710290dfa41854647e7
Ric <rohins@ubuntu.members.linode.com>
2016-07-13 21:58:32 +0000

    Added documentation for Battle.php

--- app/Battle.php
+++ app/Battle.php
@@ -10,6 +10,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Battle extends Model
 {
+    /** 
+     * Starts a battle in the room. It sets the battle
+     * parameter to 1 and sets the health for both 
+     * players to 3.
+     * @param string $room The room.
+     **/
     public static function StartBattle($room)
     {
         Redis::set($room.':battle', 1);
@@ -17,6 +23,13 @@ class Battle extends Model
         Health::set($room, 2, 3);
     }
 
+    /** 
+     * Ends a battle and cleans up the Redis database.
+     * Delete the battle parameter. Unsets the ready
+     * status for both users. Deletes the health
+     * parameters. Deletes the attack parameters.
+     * @param string $room The room.
+     **/
     public static function EndBattle($room)
     {
         Redis::del($room.':battle');
@@ -28,22 +41,44 @@ class Battle extends Model
         Attack::delete($room, 2);
     }
 
+    /**
+     * Checks if there is a battle currently in 
+     * progress in the room.
+     * @param string $room The room.
+     * @return int 1 or null.
+     **/
     public static function IsBattleInRoom($room)
     {
         $response =  Redis::get($room.':battle');
         return compact('response');
     }
 
+    /**
+     * Sets the ready parameter for a seat in a room.
+     * @param string $room The room.
+     * @param int $seat The seat.
+     **/
     public static function SetReady($room, $seat)
     {
         Redis::set($room.':'.$seat.':ready', 1);
     }
 
+    /**
+     * Delets the ready parameter for a seat in a room.
+     * @param string $room The room.
+     * @param int $seat The seat.
+     **/
     public static function SetNotReady($room, $seat)
     {
         Redis::del($room.':'.$seat.':ready');
     }
 
+    /**
+     * Gets the ready status for both seats in a room.
+     * @param string $room The room.
+     * @return array Array of Redis results for the 
+     *  ready parameters.
+     **/
     public static function GetReady($room)
     {
         $ready1 = Redis::get($room.':1:ready');
